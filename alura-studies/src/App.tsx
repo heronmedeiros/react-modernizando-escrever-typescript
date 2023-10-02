fc0222eb4c99f4eabcaab511875fca6fe3a498c5
Heron Medeiros <heron.medeiros@gmail.com>
2023-10-02 02:44:02 -0300

    Initial Commit

---
+++ alura-studies/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Botao from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Botao />
+    </div>
+  );
+}
+
+export default App;
