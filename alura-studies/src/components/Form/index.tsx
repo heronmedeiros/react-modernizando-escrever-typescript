be6794cde9e960e063f363eca23d3e4ab1c5d972
Heron Medeiros <heron.medeiros@gmail.com>
2023-10-02 04:23:09 -0300

    Adde some styles

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Botao from "../Button";
+import './Form.scss';
 
 class Form extends React.Component {
   render() {
     return (
-      <form>
-        <div>
+      <form className="novaTarefa">
+        <div className="inputContainer">
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
@@ -16,7 +17,7 @@ class Form extends React.Component {
           />
         </div>
 
-        <div>
+        <div className="inputContainer">
           <label htmlFor="tempo">Tempo</label>
           <input
             type="time"
--- alura-studies/src/components/List/index.tsx
+++ alura-studies/src/components/List/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './List.scss';
 
 function List() {
   const tarefas = [
@@ -7,11 +8,11 @@ function List() {
     {tarefa: 'Typescript', tempo: '03:00:00'},
   ];
   return (
-    <aside>
+    <aside className="listaTarefas">
       <h2>Estudos do dia</h2>
       <ul>
         { tarefas.map( (item, index) => (
-          <li key={index}>
+          <li key={index} className="item">
             <h3>{ item.tarefa }</h3>
             <span>{ item.tempo }</span>
         </li>
